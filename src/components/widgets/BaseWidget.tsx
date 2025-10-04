648ead024af2415fa7b3a361dc8e574d651f3777
Denis <devorlich@gmail.com>
2025-10-04 14:19:42 +0300

    feat: add progress widget

--- src/components/widgets/BaseWidget.tsx
+++ src/components/widgets/BaseWidget.tsx
@@ -1,29 +1,140 @@
-import React, { FC } from "react";
-import styled from "styled-components";
-import { useTheme } from "styled-components";
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { useTheme } from 'styled-components';
 import './BaseWidget.scss';
 
 const WidgetBody = styled.div`
     height: 350px;
     background-color: ${({ theme }) => theme.bgSecond};
     border-radius: 15px;
+    padding: 30px 40px;
+    max-width: 800px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const PercentageValue = styled.span<{ circularProgressSize: number }>`
+    font-size: 3.2rem;
+    font-weight: 700;
+    font-family: 'Ubuntu';
+    position: absolute;
+    top: ${({ circularProgressSize }) => `${(circularProgressSize - 33) / 2 + 'px'}`};
+    left: ${({ circularProgressSize }) => `${(circularProgressSize - 66) / 2 + 'px'}`};
+`;
+
+const InfoWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 350px;
+    height: 65px;
+    padding: 0 40px;
+`;
+
+const Info = styled.div`
+    height: 100%;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
-    padding: 20px;
+
+    span {
+        font-size: 2rem;
+        font-weight: 500;
+    }
 `;
+
+const CircularProgress: React.FC<CircularProgressProps> = ({
+    value,
+    size = 210,
+    strokeWidth = 20,
+    progressColor = '#37B4C8'
+}) => {
+    const radius = (size - strokeWidth) / 2; // Радиус круга
+    const circumference = 2 * Math.PI * radius; // Длина окружности
+    const offset = circumference - (value / 100) * circumference; // Смещение прогресса
+
+    const theme = useTheme();
+
+    if (value >= 85) {
+        progressColor = theme.error;
+    }
+
+    return (
+        <div className="progress_bar">
+            <svg width={size + 10} height={size + 10}>
+                {/* Фильтр для свечения */}
+                <defs>
+                    <filter id="glow">
+                        <feGaussianBlur stdDeviation="5" result="coloredBlur" />
+                        <feMerge>
+                            <feMergeNode in="coloredBlur" />
+                            <feMergeNode in="SourceGraphic" />
+                        </feMerge>
+                    </filter>
+                </defs>
+
+                {/* Прогресс */}
+                <circle
+                    cx={size / 2}
+                    cy={size / 2}
+                    r={radius}
+                    stroke={progressColor}
+                    strokeWidth={strokeWidth}
+                    fill="none"
+                    strokeDasharray={circumference}
+                    strokeDashoffset={offset}
+                    filter="url(#glow)" // Применяем свечение
+                    transform={`rotate(270 ${size / 2} ${size / 2})`}
+                />
+            </svg>
+        </div>
+    );
+};
 
 const BaseWidget: FC = () => {
     const theme = useTheme();
+    const circularProgressSize = 210;
 
     return (
         <WidgetBody>
-            <CircularProgress
-                className="progress_bar"
-                progressColor={theme.accent}
-                dimAccent={theme.dimAccent}
-                value={90}
-            />
+            <div className='progress_widget_left'>
+                <div className="progress_bar_wrapper">
+                    <CircularProgress
+                        progressColor={theme.accent}
+                        value={50}
+                        size={circularProgressSize}
+                    />
+                    <PercentageValue circularProgressSize={circularProgressSize}>38%</PercentageValue>
+                </div>
+                <InfoWrapper>
+                    <Info>
+                        <span>Потрачено</span>
+                        <span style={{color: theme.error}}>2436 $</span>
+                    </Info>
+                    <Info>
+                        <span>Остаток</span>
+                        <span style={{color: theme.accent}}>3640 $</span>
+                    </Info>
+                </InfoWrapper>
+            </div>
+            <ul className='progress_widget_right'>
+                <li>
+                    <span>Развлечения</span>
+                    <span>304 $</span>
+                </li>
+                <li>
+                    <span>Спорт</span>
+                    <span>124 $</span>
+                </li>
+                <li>
+                    <span>Еда</span>
+                    <span>259 $</span>
+                </li>
+                <li>
+                    <span>Машина</span>
+                    <span>201 $</span>
+                </li>
+            </ul>
         </WidgetBody>
     );
 };
@@ -32,66 +143,7 @@ interface CircularProgressProps {
     value: number; // Прогресс в процентах
     size?: number; // Размер круга
     strokeWidth?: number; // Толщина линии
-    dimAccent?: string; // Цвет фона круга
     progressColor?: string; // Цвет прогресса
-    className: string
 }
 
-const CircularProgress: React.FC<CircularProgressProps> = ({
-    value,
-    size = 220,
-    strokeWidth = 20,
-    progressColor = "#37B4C8",
-    dimAccent,
-    className
-}) => {
-    const radius = (size - strokeWidth) / 2 - 10; // Радиус круга
-    const circumference = 2 * Math.PI * radius; // Длина окружности
-    const offset = circumference - (value / 100) * circumference; // Смещение прогресса
-    
-    const theme = useTheme();
-
-    if (value >= 85) {
-        progressColor = theme.error
-    }
-
-    return (
-        <svg width={size} height={size} className={className}>
-            {/* Фильтр для свечения */}
-            <defs>
-                <filter id="glow">
-                    <feGaussianBlur stdDeviation="5" result="coloredBlur" />
-                    <feMerge>
-                        <feMergeNode in="coloredBlur" />
-                        <feMergeNode in="SourceGraphic" />
-                    </feMerge>
-                </filter>
-            </defs>
-
-            {/* Фон круга */}
-            <circle
-                cx={size / 2}
-                cy={size / 2}
-                r={radius}
-                stroke={dimAccent}
-                strokeWidth={strokeWidth}
-                fill="none"
-                strokeLinecap="round"
-            />
-
-            {/* Прогресс */}
-            <circle
-                cx={size / 2}
-                cy={size / 2}
-                r={radius}
-                stroke={progressColor}
-                strokeWidth={strokeWidth}
-                fill="none"
-                strokeDasharray={circumference}
-                strokeDashoffset={offset}
-                filter="url(#glow)" // Применяем свечение
-            />
-        </svg>
-    );
-};
 export default BaseWidget;
--- src/styled.ts
+++ src/styled.ts
@@ -1,6 +1,17 @@
-import { DefaultTheme } from "styled-components"
+export interface BaseTheme {
+    readonly accent: '#2ED9E5',
+    readonly dimAccent: '#1F9199',
+    readonly btnAccent: '#29C1CC',
+    readonly error: '#CB2020',
+    readonly success: "#14BA1A",
+    readonly bgMain: '#0F0F0F',
+    readonly bgSecond: '#232626',
+    readonly text: '#DFE5E5',
+    readonly disable: '#9E9E9E',
+    readonly input: '#393D3D'
+}
 
-export const baseTheme: DefaultTheme = {
+export const baseTheme: BaseTheme = {
     accent: '#2ED9E5',
     dimAccent: '#1F9199',
     btnAccent: '#29C1CC',
@@ -9,5 +20,8 @@ export const baseTheme: DefaultTheme = {
     bgMain: '#0F0F0F',
     bgSecond: '#232626',
     text: '#DFE5E5',
-    disable: '#9E9E9E'
+    disable: '#9E9E9E',
+    input: '#393D3D'
 }
+
+
